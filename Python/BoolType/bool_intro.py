ef72dd936670b517fe5c3e62e14c047387a47109
techtorialdevopsb2 <122166999+techtorialdevopsb2@users.noreply.github.com>
2023-09-25 19:07:07 -0500

    Bool Notes Added

---
+++ Python/BoolType/bool_intro.py
@@ -0,0 +1,35 @@
+
+#Creating a boolean variable
+is_Rainy = False 
+print(is_Rainy)
+#Let's look at the type of the variable 
+print(type(is_Rainy)) # <class 'bool'>
+
+is_September = True 
+print(is_September)
+#Let's look at the type of the variable 
+print(type(is_September)) # <class 'bool'>
+
+# Code below will generate an error because false is not 
+# python reserved. False is not same as false
+#is_updated = false
+
+#
+a = 0
+false = a
+new = False
+print ("The variable new is:",new) 
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Python/NumericalTypes/Casting/casting_intro.py
+++ Python/NumericalTypes/Casting/casting_intro.py
@@ -1,20 +1,22 @@
 # In python we can also convert data types to each other 
 # using constructor function of that specific type. 
 
-var1 = 5.1
+var1 = 5.9
 print(type(var1)) # <class 'float'>
-
-var1 = int(var1)
+print(var1) # 5.9
+var1 = int(var1) #making integer just removes the decimal point
 print(type(var1)) # <class 'int'>
-
+print(var1) # 5
 var1 = float(var1)
 print(type(var1)) # <class 'float'>
+print(var1) # 5.0
 
+s = "6"
+print(type(s))
 
+s = int(s)
 
-
-
-
+print(type(s))
 
 
 
